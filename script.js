09b1496f5becfdc8a0f27cdf987b6ea66ee7e06f
Kavindu Munugoda <102134818+munu832@users.noreply.github.com>
2024-08-26 07:31:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,34 +9,37 @@ window.addEventListener('load', function() {
     }, 500);
 });
 
-// Figure modal functionality
+// Key Figures functionality
 const figures = document.querySelectorAll('.figure img');
-const body = document.body;
 
 figures.forEach(figure => {
     figure.addEventListener('click', function() {
+        const link = this.getAttribute('data-link');
+        window.location.href = link;
+    });
+});
+
+// Figure modal functionality
+const body = document.body;
+figures.forEach(figure => {
+    figure.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent default click behavior
         const modal = document.createElement('div');
         modal.classList.add('modal');
-
         const modalImg = document.createElement('img');
         modalImg.src = this.getAttribute('data-full');
         modalImg.classList.add('modal-content');
-
         const closeBtn = document.createElement('span');
         closeBtn.innerHTML = '&times;';
         closeBtn.classList.add('close');
-
         modal.appendChild(modalImg);
         modal.appendChild(closeBtn);
         body.appendChild(modal);
-
         modal.style.display = 'flex';
-
         closeBtn.onclick = function() {
             modal.style.display = "none";
             body.removeChild(modal);
         }
-
         modal.onclick = function(event) {
             if (event.target === modal) {
                 modal.style.display = "none";
@@ -54,20 +57,9 @@ const imagePaths = [
     'interview3.jpg',
     // Add more image paths as needed
 ];
-
 imagePaths.forEach(path => {
     const img = document.createElement('img');
     img.src = path;
     img.alt = 'Interview Photo';
     imageGallery.appendChild(img);
-});
-
-// Key Figures functionality
-const figures = document.querySelectorAll('.figure img');
-
-figures.forEach(figure => {
-    figure.addEventListener('click', function() {
-        const link = this.getAttribute('data-link');
-        window.location.href = link;
-    });
 });
